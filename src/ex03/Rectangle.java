e7434f8c2dce1ef09ae70824dea6f7b03c8d9a03
Guilherme Luiz <margilepow1227@gmail.com>
2026-02-12 15:02:46 -0300

    New ex's

---
+++ src/ex03/Rectangle.java
@@ -0,0 +1,25 @@
+package ex03;
+
+public class Rectangle {
+
+    public double w;
+    public double h;
+
+    public double area() {
+        return w * h;
+    }
+
+    public double perimeter() {
+        return (h + h) + (w + w);
+    }
+
+    public double diagonal() {
+        return Math.sqrt(w * w + (h * h));
+    }
+
+    public String toString() {
+        return "Área = " + String.format("%.2f", area()) + "\nPerimeter: " +
+                String.format("%.2f", perimeter()) + "\nDiagonal: " + String.format("%.2f", diagonal());
+    }
+
+}
